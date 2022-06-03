2da6e5ab81a4479232077eaaa8bad11ac784e1de
Mr404NotFoundd <103042336+Mr404NotFoundd@users.noreply.github.com>
2022-06-03 23:06:52 +0700

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -2,6 +2,6 @@ import os
 os.system('git pull')
 if __name__ == "__main__":
         try:
-                __import__("igxrex").checkin()
+                __import__("igku").checkin()
         except Exception as e:
                 exit(str(e))
